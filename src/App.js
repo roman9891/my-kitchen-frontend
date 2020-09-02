ca9c2bdfa8366d60060cefdc7713102363119043
yulia <juliana.ny2008@gmail.com>
2020-09-02 18:30:08 -0400

    j

--- src/App.js
+++ src/App.js
@@ -5,13 +5,26 @@ import IngredientsContainer from "./Containers/IngredientsContainer";
 import RecipesContainer from "./Containers/RecipesContainer";
 
 class App extends React.Component {
+  state={
+    search: ""
+  }
+
+  
+  searchHandler = event =>{ 
+      this.setState({
+         search: event.target.value
+      })
+      console.log(this.state)
+
+      //place for fetch
+ }
 
   render(){
     return(
     <div>
-      <IngredientsContainer/>
+      <IngredientsContainer searchHandler = {this.searchHandler} />
       <RecipesContainer/>
-
+     
     </div>
     )
       
