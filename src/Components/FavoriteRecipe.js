4241574c2527ee016f2d1d6c29d8e0d2696f2ed1
yulia <juliana.ny2008@gmail.com>
2020-09-10 19:46:41 -0400

    trying to render instructions inside of FavirireRecipe component

--- src/Components/FavoriteRecipe.js
+++ src/Components/FavoriteRecipe.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
 
 class FavoriteRecipe extends Component {
+    
+    
     render() {
-        return ( 
+        //  const stepsArray = this.props.recipe.instructions.split('#')  //error: Can not read property of underfined.
+        //  let instructions = stepsArray.map((step) => <p>{step}</p>)
+
+        return ( console.log( this.props.recipe.instructions),
             <div id="favorite-recipe-container">
               <h2>{this.props.recipe.title}</h2> 
               <h6>{this.props.recipe.ingredients}</h6>
-              <p>{this.props.recipe.steps}</p> 
+              <p> {this.props.recipe.instructions}</p> 
             </div>
         );
     }
 }
 
 export default FavoriteRecipe;
+
